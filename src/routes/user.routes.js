f58206aa6b7b1e945bb0233c6157b892b79d3349
Sparsh-Jain-531 <sparshjain531@gmail.com>
2025-03-08 12:52:08 +0530

    feat: add user channel profile and watch history retrieval features

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,29 +1,52 @@
 import { Router } from "express";
-import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { isAuthenticated } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(upload.fields([
-    { name: 'avatar', maxCount: 1 },
-    { name: 'coverImage', maxCount: 1 }
-]),registerUser)
+router.route("/register").post(
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  registerUser
+);
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
-router.route("/logout").get(isAuthenticated, logoutUser)
+router.route("/logout").get(isAuthenticated, logoutUser);
 
-router.route("/refresh-token").get(refreshAccessToken)
+router.route("/refresh-token").get(refreshAccessToken);
 
-router.route("/change-password").post(isAuthenticated, changeCurrentPassword)
+router.route("/change-password").post(isAuthenticated, changeCurrentPassword);
 
-router.route("/user-details").get(isAuthenticated, getCurrentUser)
+router.route("/user-details").get(isAuthenticated, getCurrentUser);
 
-router.route("/update-account").post(isAuthenticated, updateAccountDetails)
+router.route("/update-account").patch(isAuthenticated, updateAccountDetails);
 
-router.route("/update-avatar").post(isAuthenticated, updateUserAvatar)
+router
+  .route("/update-avatar")
+  .patch(isAuthenticated, upload.single("avatar"), updateUserAvatar);
 
-router.route("/update-cover").post(isAuthenticated, updateUserCoverImage)
+router
+  .route("/update-cover")
+  .patch(isAuthenticated, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(isAuthenticated, getUserChannelProfile);
+
+router.route("/history").get(isAuthenticated, getWatchHistory);
 
 export default router;
